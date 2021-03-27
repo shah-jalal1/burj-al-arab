8328aa0a9d2c8df669801c78aa0f525b25ff6113
shahjalalcse <shah-jalal1>
2021-03-27 13:54:16 +0600

    start

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA_NqKhxS_Wo8gd9UM5eQgXWy1zHWzmeWM",
+  authDomain: "burj-al-arab-248d0.firebaseapp.com",
+  projectId: "burj-al-arab-248d0",
+  storageBucket: "burj-al-arab-248d0.appspot.com",
+  messagingSenderId: "879589904237",
+  appId: "1:879589904237:web:e0f7dfc4856e475f015464"
+  };
+  export default firebaseConfig;
